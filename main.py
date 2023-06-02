cba7d0379ede4024faa341c9fcaa3921e9f5b11e
Maxremmo <max.remme@gmail.com>
2023-06-02 16:10:50 +0200

    improved readability/functionality

--- main.py
+++ main.py
@@ -63,111 +63,149 @@ class Summoner:
         participant_index = match['metadata']['participants'].index(self.puuid)
     
         return participant_index
+    
+class Game():
 
-       
+    def __init__(self, match):
+        self.match = match
+        
+        
+    def get_kills(self):
+
+        kills = self.match['info']['participants'][participant_index]['kills']
+        
+        return kills
+    
+    def get_deaths(self):
+        
+        deaths = self.match['info']['participants'][participant_index]['deaths']
+        
+        return deaths
+    
+    def get_assists(self):
+        
+        assists = self.match['info']['participants'][participant_index]['assists']
+        
+        return assists
+    
+    def get_takedowns(self):
+        
+        takedowns = self.match['info']['participants'][participant_index]['challenges']['takedowns']
+        
+        return takedowns
+    
+    def get_multikills(self):
+        
+        multikills = self.match['info']['participants'][participant_index]['challenges']['multikills']
+        
+        return multikills
+    
+    def get_visionScore(self):
+        
+        visionScore = self.match['info']['participants'][participant_index]['visionScore']
+        
+        return visionScore
+    
+    def get_kda(self):
+        kda = (self.get_kills() + self.get_assists / self.get_deaths)
+
+        return kda
+    
+    def get_gameMode (self):
+        
+        mode = self.match['info']['gameMode']
+
+        return mode
+    
+    def get_championName (self):
+        
+        champName = self.match['info']['participants'][participant_index]['championName']
+
+        return champName
+    
+    def get_role (self):
+        
+        role = self.match['info']['participants'][participant_index]['individualPosition']
+
+        return role
+    
+    def get_win (self):
+        
+        won = self.match['info']['participants'][participant_index]['win']
+
+        return won
+
 #initalize account 
 active_account = Summoner(api_key)
-
 active_history = active_account.get_history()
-
-print(active_history)
-
 
 #initialize counters (will clean this up)
 
-player1_total_kills = 0
-    
-player1_total_deaths = 0
-    
-player1_multikills = 0
-    
-player1_total_takedowns = 0
-    
-player1_total_vision_score = 0
-    
-player1_wins = 0
-    
-player1_losses = 0
+player_total_kills = 0
+player_total_deaths = 0
+player_multikills = 0
+player_total_takedowns = 0
+player_total_vision_score = 0
+player_wins = 0
+player_losses = 0
 
 #loop over games, each time getting match data and participant index to track player,
 #display Game type, Role, Champion, Kills/Deaths/Assists/ Wins and Loss, Multikills, Takedowns and Vision score
 for i in range(len(active_history)):
-        
+
     current_game = active_account.get_match(active_history[i])
-    
+
     participant_index = active_account.get_participant_index(current_game)
-    
-    print("GAME " + str(i) + ":" + "\n" + "Game Type: " + current_game['info']['gameMode'])
-    
-    print("Role: " + current_game['info']['participants'][participant_index]['individualPosition'])
-    
-    player1_champion = current_game['info']['participants'][participant_index]['championName']
 
-    print("Champion played: " + current_game['info']['participants'][participant_index]['championName'])
+    active_game = Game(current_game)
+    
+    print(f"GAME {i} : {active_game.get_gameMode()}")
+    
+    print(f"Role:  {active_game.get_role()}")
 
-    print(
-        "KDA: " + str(current_game['info']['participants'][participant_index]['kills']) + "/" +
+    print(f"Champion played: {active_game.get_championName()}")
 
-        str(current_game['info']['participants'][participant_index]['deaths']) + "/" +
+    print(f"KDA: {active_game.get_kills()}/{active_game.get_deaths()}/{active_game.get_assists()}")
+    
+    player_won = active_game.get_win()
+    
+    if player_won == True:
+        player_wins +=1
+        
+    else:
+        player_losses += 1
+    
+    print(f"Wins : {player_wins}")
+    
+    print(f"Losses : {player_losses}")
+    
+    print(f"Game won:{active_game.get_win()} ")
+    
+    kills = active_game.get_kills()
+    
+    player_total_kills = player_total_kills + kills
+    
+    deaths = active_game.get_deaths()
+    
+    player_total_deaths = player_total_deaths + deaths
+    
+    multikills = active_game.get_multikills()
+    
+    print(f"Multikills: {active_game.get_multikills()}" )
+    
+    takedowns = active_game.get_takedowns()
 
-        str(current_game['info']['participants'][participant_index]['assists'])
-        
-          
-          )
+    print(f"Takedowns: {active_game.get_takedowns()}" )
     
-    player1_won = current_game['info']['participants'][participant_index]['win']
+    vision_score = active_game.get_visionScore()
     
-    
-    if player1_won == True:
-        
-        player1_wins +=1
-        
-    elif player1_won == False:
-        
-        player1_losses += 1
-    
-    print(f"Wins : {player1_wins}")
-    
-    print(f"Wins : {player1_losses}")
-    
-    print("Game won: " + str(current_game['info']['participants'][participant_index]['win']))
-    
-    player1_kills = current_game['info']['participants'][participant_index]['kills']
-    
-    player1_total_kills = player1_total_kills + player1_kills
-    
-    player1_deaths = current_game['info']['participants'][participant_index]['deaths']
-    
-    player1_total_deaths = player1_total_deaths + player1_deaths
-    
-    player1_multikills = str(current_game['info']['participants'][participant_index]['challenges']['multikills'])
-    
-    print("Multikills: " + str(current_game['info']['participants'][participant_index]['challenges']['multikills']))
-    
-    player1_takedowns = str(current_game['info']['participants'][participant_index]['challenges']['takedowns'])
-    
-    print("Takedowns: " + str(current_game['info']['participants'][participant_index]['challenges']['takedowns']))
-    
-    player1_vision_score = str(current_game['info']['participants'][participant_index]['visionScore'])
-    
-    print("Vision Score: " + str(current_game['info']['participants'][participant_index]['visionScore']) + "\n")
+    print(f"Vision Score: {active_game.get_visionScore()} " )
 
     print("---- ---- ---- ----\n")
 
     time.sleep(1)
-
 #display match facts over 20 games     
-print( "Over 20 games: \n" + 
-    "Total Kills: " + str(player1_total_kills) +
-      
-      "\nTotal Deaths: " + str(player1_total_deaths) +
-      
-      "\nK/D Ratio: " + str(player1_total_kills/player1_total_deaths)
-
-
-
-
-
+print( f"Over 20 Games: \n Total Deaths: {player_total_deaths} \n Total Kills: {player_total_kills} KD/A Ratio: {active_game.get_kda}"
 )
 
 
